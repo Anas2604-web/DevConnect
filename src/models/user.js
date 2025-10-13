e785692d5328a2d8f266d5c5f0a05326d8627104
Anas Khan <annaasskhan6@gmail.com>
2025-10-14 02:47:27 +0530

    Added some Validations in Schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,17 +3,44 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30
     },
     lastName: {
         type: String,
     },
     email: {
-        type: String,   
+        type: String,
+        required: true,
+        unique: true,   
     },
     password: {
         type: String,
-    }
-});
+        required: true,
+        minlength: 6,
+        maxlength: 100
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        min: 18,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    skills: {
+        type: [String],
+        default: []
+    },
+
+}, {timestamps: true}
+);
 
 const User = mongoose.model('User', userSchema);
 
