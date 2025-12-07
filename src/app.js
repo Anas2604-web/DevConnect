00a9ed433799afbb6b1c5126eb6a4dd2428e4946
Anas Khan <annaasskhan6@gmail.com>
2025-12-07 23:23:49 +0530

    Added Cookies and generated jwt token for profile

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@
  const User = require("./models/user");
  const validateSignUp = require("./utils/validation");
  const bcrypt = require('bcrypt');
+ const jwt = require('jsonwebtoken');
+ const cookieParser = require('cookie-parser');
 
  const app = express();
 
+ app.use(cookieParser());
  app.use(express.json());
 
  app.post("/signup",async(req,res)=>{
@@ -34,6 +37,59 @@
    }
 
  });
+
+ app.post("/login", async(req,res)=> {
+   try {
+         const {email, password} = req.body;
+
+         const user = await User.findOne({email});
+         if(!user) {
+           throw new Error("Invalid credentials");
+         }
+
+         const isPasswordValid = await bcrypt.compare(password, user.password);
+
+         if(isPasswordValid) {
+           const token = await jwt.sign({_id: user._id}, "DEV@Op&8788");
+
+           res.cookie("token", token);
+
+           res.send("Login successful");
+         }
+         else {
+           throw new Error("Invalid credentials");
+         }
+          
+   }
+   catch(err) {
+      res.status(500).send("Error during login");
+   } 
+ })
+
+ app.get("/profile", async(req,res) => {
+     try {
+         const cookies = req.cookies;
+          const {token} = cookies;
+            if(!token) {
+               return res.status(401).send("Unauthorized: No token provided");
+            }
+
+          const decoded = jwt.verify(token, "DEV@Op&8788");
+            const userId = decoded._id;
+            console.log(userId);
+
+            const user = await User.findById(userId).select("-password -__v");
+            if(!user) {
+               return res.status(404).send("User not found");
+            }
+            res.json(user);
+
+
+     }
+     catch(err) {
+         res.status(500).send("Error fetching profile");
+     }
+ })
 
  app.patch("/user/:userId", async(req,res) => {
      try {
