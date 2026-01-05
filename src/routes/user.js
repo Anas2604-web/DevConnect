77818f6253a14b4497c43c29dfaa4fd2c65bab7f
Anas Khan <annaasskhan6@gmail.com>
2026-01-05 20:41:25 +0530

    Created the api to get the users connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -24,4 +24,35 @@ userRouter.get("/user/requests/received", userAuth,  async (req,res) => {
         }
 })
 
+userRouter.get("/user/connections", userAuth,  async (req,res) => {
+    try {
+       const loggedInUser = req.user;
+
+       const requests = await ConnectionRequest.find({
+        $or: [
+        { toUserId: loggedInUser._id, status:  "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" }
+        ]
+        })
+        .populate(
+        "fromUserId", 
+        "firstName  lastName  photoUrl  about  gender  age city  skills " )
+        .populate(
+        "toUserId",
+        "firstName  lastName  photoUrl  about  gender  age city  skills " );
+
+        const data = requests.map((row) => {
+          if(row.fromUserId === loggedInUser._id) {
+            return row.toUserId;
+          }
+          return row.fromUserId;
+        })
+
+        res.json({ data  })
+    }
+        catch(err) {
+          res.status(400).send("ERROR" + err.message);
+        }
+})
+
 module.exports = userRouter
