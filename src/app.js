837985dc2cc475d19f985fe730f2d186c2f97fb1
Anas Khan <annaasskhan6@gmail.com>
2026-02-12 21:37:11 +0530

    Fixed Routing Issues

--- src/app.js
+++ src/app.js
@@ -19,10 +19,10 @@ app.use(cookieParser());
 app.use(express.json());
 
 
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-app.use("/", userRouter);
+app.use("/api", authRouter);
+app.use("/api", profileRouter);
+app.use("/api", requestRouter);
+app.use("/api", userRouter);
 
 connectDB()
   .then(() => {
